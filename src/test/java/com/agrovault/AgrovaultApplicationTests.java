f942af950b626ea1053de8ab6557ea229623b48f
Pratik Mulay <pratikmulay17@gmail.com>
2026-03-18 22:43:15 +0530

    initial unit tests for services

---
+++ src/test/java/com/agrovault/AgrovaultApplicationTests.java
@@ -0,0 +1,12 @@
+package com.agrovault;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+class AgrovaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
