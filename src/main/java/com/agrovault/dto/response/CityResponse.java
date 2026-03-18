d5aec0ace3f87b689458a63a8171003619b126d1
Pratik Mulay <pratikmulay17@gmail.com>
2026-03-18 22:40:03 +0530

    DTO for city data responses

---
+++ src/main/java/com/agrovault/dto/response/CityResponse.java
@@ -0,0 +1,15 @@
+package com.agrovault.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CityResponse {
+    private Integer id;
+    private String name;
+}
