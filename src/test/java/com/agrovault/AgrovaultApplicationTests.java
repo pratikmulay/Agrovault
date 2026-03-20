74df40980d9bdc0849f324a4c7e6ed8a0a528137
Pratik Mulay <pratikmulay17@gmail.com>
2026-03-21 00:40:46 +0530

    Tests the  application context and test resources

--- src/test/java/com/agrovault/AgrovaultApplicationTests.java
+++ src/test/java/com/agrovault/AgrovaultApplicationTests.java
@@ -2,8 +2,10 @@ package com.agrovault;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AgrovaultApplicationTests {
 
     @Test
